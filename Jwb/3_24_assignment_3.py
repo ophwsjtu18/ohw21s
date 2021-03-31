446ac8b734974ea8bb2e862d324161441460dc19
Jinwb2019 <59530895+Jinwb2019@users.noreply.github.com>
2021-03-31 17:17:32 +0800

    Create 3_24_assignment_3.py

---
+++ Jwb/3_24_assignment_3.py
@@ -0,0 +1,14 @@
+#在图片左上角画9个绿色矩形，线宽3个像素
+import numpy as np
+import cv2
+
+img = cv2.imread('siren.jpg' , 1)
+for x in range(3):
+    for y in range(3):
+        cv2.line(img , ((x*200),(y*200)),((200+200*x),(200*y)),(0,255,0),3)
+        cv2.line(img , ((x*200),(y*200)),((200*x),(200*y+200)),(0,255,0),3)
+        cv2.line(img , ((x*200+200),(y*200)),((200+200*x),(200*y+200)),(0,255,0),3)
+        cv2.line(img , ((x*200),(y*200+200)),((200+200*x),(200*y+200)),(0,255,0),3)
+cv2.imshow('res',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
